1ed9ba65b493351a44f764ef24174b45d0ab6584
Tayyeb Shahzad <tayyebbutt966@gmail.com>
2024-10-26 10:42:50 +0500

    prisma setup

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,43 +1,49 @@
+"use client";
+
 import Link from 'next/link'
 import React from 'react'
 import Image from 'next/image'
 import bug_logo from '../../assets/bug-report.png'
 import { lexendDeca } from '../ui/fonts'
+import { usePathname } from 'next/navigation'
+import classNames from 'classnames';
 
 const Navbar = () => {
 
+  const currentpath = usePathname();
+  console.log(currentpath);
 
   const links = [
     {
       label: "Dashboard",
-      href: '/'
+      Link: '/'
     },
     {
       label: "Issues",
-      href: '/'
+      Link: '/issues'
     }
   ]
 
 
 
   return (
-    <nav className='flex space-x-6 border-b mb-5 px-5 h-20 items-center'>
+    <nav className='flex space-x-16 border-b mb-5 px-5 h-20 items-center'>
       <Link className={`flex items-center ${lexendDeca.className}`} href="/">
       <Image src={bug_logo} height={40} width={40} alt='logo'/>
       </Link>
       <ul className='flex space-x-4'>
 
         {links.map(link =>
-          
-
-          <Link key={link.href} className={`${lexendDeca.className} text-zinc-400 hover:text-black transition duration-300`} href={link.href}>{link.label}</Link>
+          <Link key={link.Link}
+            className={classNames({
+              'text-zinc-900': link.Link === currentpath,
+              'text-zinc-500': link.Link !== currentpath,
+              'hover:text-zinc-800 transition-colors': true
+            })}
+            href={link.Link}>{link.label}
+          </Link>
         
         )}
-
-        
-
-        {/* <li><Link className={`${lexendDeca.className} text-zinc-400 hover:text-black transition duration-300`} href="/">Dashboard</Link></li>
-        <li><Link className={`${lexendDeca.className} text-zinc-400 hover:text-black duration-300`} href="/">Issue's</Link></li> */}
       </ul>
     </nav>
   )
