e12a76ac2a280e654cf8399aad524fded4f29031
Tayyeb Shahzad <tayyebbutt966@gmail.com>
2024-11-02 19:37:22 +0500

    add a markdown

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,8 +2,11 @@
 import React from 'react'
 import { TextField } from '@radix-ui/themes'
 import { lexendDeca } from '@/app/ui/fonts'
-import { TextArea } from '@radix-ui/themes'
+// import { TextArea } from '@radix-ui/themes'
 import { Button } from '@radix-ui/themes'
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+
 
 const NewIssuePage = () => {
   return (
@@ -11,7 +14,7 @@ const NewIssuePage = () => {
       <TextField.Root>
               <TextField.Input className={`${lexendDeca.className}`} placeholder="Title" />
           </TextField.Root>
-          <TextArea className={`${lexendDeca.className}`} placeholder='Description' />
+          <SimpleMDE className={`${lexendDeca.className}`} placeholder='Description' />
           <Button className={`${lexendDeca.className}`}>
               Submit New Issue
           </Button>
