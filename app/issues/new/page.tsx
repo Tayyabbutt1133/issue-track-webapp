09660ad4b5d1ca3b212ae6887791869e861c20e7
Tayyeb Shahzad <tayyebbutt966@gmail.com>
2024-11-04 12:09:45 +0500

    handle form submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,25 +1,50 @@
-"use client"
-import React from 'react'
-import { TextField } from '@radix-ui/themes'
-import { lexendDeca } from '@/app/ui/fonts'
-// import { TextArea } from '@radix-ui/themes'
-import { Button } from '@radix-ui/themes'
+"use client";
+import React from "react";
+import { TextField } from "@radix-ui/themes";
+import { lexendDeca } from "@/app/ui/fonts";
+import { Button } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
-
-const NewIssuePage = () => {
-  return (
-    <div className='max-w-xl space-y-3'>
-      <TextField.Root>
-              <TextField.Input className={`${lexendDeca.className}`} placeholder="Title" />
-          </TextField.Root>
-          <SimpleMDE className={`${lexendDeca.className}`} placeholder='Description' />
-          <Button className={`${lexendDeca.className}`}>
-              Submit New Issue
-          </Button>
-    </div>
-  )
+interface IssueForm {
+  title: string;
+  description: string;
 }
 
-export default NewIssuePage
+const NewIssuePage = () => {
+
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const router = useRouter();
+  // console.log(register("title"));
+  return (
+    <form className="max-w-xl space-y-3" onSubmit={handleSubmit( async (data) => {
+      await axios.post('/api/issues', data);
+      router.push('/issues');
+    })}>
+      <TextField.Root>
+        <TextField.Input
+          className={`${lexendDeca.className}`}
+          placeholder="Title"
+          {...register("title")} />
+      </TextField.Root>
+
+      {/* we need to connect SimpleMDE with controller to make that bridge pass data */}
+      <Controller
+        name="description"
+        control={control}
+        render={({ field: { onChange, onBlur} }) => (
+          <SimpleMDE
+            onChange={onChange} // send value to hook form
+            onBlur={onBlur} // notify when input is touched/blur
+          />
+        )} {...register} />
+      
+      <Button className={`${lexendDeca.className}`}>Submit New Issue</Button>
+    </form>
+  );
+};
+
+export default NewIssuePage;
