c29295a6f5dff5f87d3c2f32218a8d76b33d6d97
Tayyeb Shahzad <tayyebbutt966@gmail.com>
2024-11-05 12:15:49 +0500

    zod client validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,28 +1,48 @@
 "use client";
 import React from "react";
-import { TextField } from "@radix-ui/themes";
+import { Text, TextField } from "@radix-ui/themes";
 import { lexendDeca } from "@/app/ui/fonts";
 import { Button } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
+import {toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { zodResolver } from '@hookform/resolvers/zod'
+import { createIssueSchema } from "@/app/ValidationSchemas";
+import {z} from 'zod'
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>
 
 const NewIssuePage = () => {
 
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const { register, control, handleSubmit, formState:{ errors }} = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema)
+  });
   const router = useRouter();
-  // console.log(register("title"));
+  const [Error, setError] = useState("");
   return (
-    <form className="max-w-xl space-y-3" onSubmit={handleSubmit( async (data) => {
-      await axios.post('/api/issues', data);
-      router.push('/issues');
+    <>
+    <form className="max-w-xl space-y-3" onSubmit={handleSubmit(async (data) => {
+      try {
+        await axios.post('/api/issues', data);
+        router.push('/issues');
+      } catch (error) {
+        setError('Fill the form completely !');
+        toast.error("You did not complete form !", {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+        })
+      }
     })}>
       <TextField.Root>
         <TextField.Input
@@ -31,6 +51,8 @@ const NewIssuePage = () => {
           {...register("title")} />
       </TextField.Root>
 
+        {/* Validating with zod */}
+  {errors.title && <Text color="red" as="p">{errors.title.message}</Text>}
       {/* we need to connect SimpleMDE with controller to make that bridge pass data */}
       <Controller
         name="description"
@@ -40,10 +62,15 @@ const NewIssuePage = () => {
             onChange={onChange} // send value to hook form
             onBlur={onBlur} // notify when input is touched/blur
           />
-        )} {...register} />
-      
+          )} {...register} />
+        
+        {/* Validating with zod */}
+        {errors.description && <Text color="red">{errors.description.message}</Text>}
+
       <Button className={`${lexendDeca.className}`}>Submit New Issue</Button>
     </form>
+      <ToastContainer />
+      </>
   );
 };
 
